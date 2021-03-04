2436401aab79e8498ec6cdef79d37055b1bbbefe
Keido Sumi <delta0726@nta.pial.jp>
2021-03-05 06:37:01 +0900

    Add slide link

---
+++ blog/slide_link.R
@@ -0,0 +1,26 @@
+# ***********************************************************************************************
+# Title     : 参考スライド
+# Objective : TODO
+# Created by: Owner
+# Created on: 2021/01/24
+# URL       :
+# ***********************************************************************************************
+
+
+# 協調フィルタリング入門
+# https://www.slideshare.net/hoxo_m/ss-53305070
+
+# 協調フィルタリングを利用した推薦システムを作ってみた
+# https://www.slideshare.net/masayuki1986/recommendation-ml
+
+
+# Collaborativefilteringwith r
+# https://www.slideshare.net/teitonakagawa/collaborativefilteringwith-r
+
+
+# ベイジアンネットとレコメンデーション
+# https://www.slideshare.net/hamadakoichi/ss-4552939
+
+
+# 協調フィルタリング技術を掘り下げる--ECサイトのレコメンド技術を考える（3）
+# https://japan.zdnet.com/article/20374062/
